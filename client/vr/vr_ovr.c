84e64dd519a7ee46809a0eff9a63beefc9530ced
Luke Groeninger <luke@dghost.net>
2014-06-01 01:46:42 -0600

    WIP - enumerate hmd/sensor with C API

--- client/vr/vr_ovr.c
+++ client/vr/vr_ovr.c
@@ -1,7 +1,7 @@
 #include "include/vr.h"
 #include "include/vr_ovr.h"
-#include "include/vr_libovr.h"
-
+#include "OVR_CAPI.h"
+#include <SDL.h>
 
 void VR_OVR_GetHMDPos(int32_t *xpos, int32_t *ypos);
 void VR_OVR_GetState(vr_param_t *state);
@@ -29,13 +29,15 @@ cvar_t *vr_ovr_supersample;
 cvar_t *vr_ovr_latencytest;
 cvar_t *vr_ovr_enable;
 
-static ovr_settings_t vr_ovr_settings = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, { 0, 0, 0, 0,}, { 0, 0, 0, 0,}, "", ""};
-
 
 ovr_attrib_t ovrConfig = {0, 0, 0, 0, 0, 0, 0, { 0, 0, 0, 0,}, { 0, 0, 0, 0,}};
 
 vr_param_t ovrState = {0, 0, 0, 0, 0, 0};
 
+ovrHmd hmd;
+ovrHmdDesc hmdDesc; 
+
+static double prediction_time;
 
 hmd_interface_t hmd_rift = {
 	HMD_RIFT,
@@ -54,45 +56,45 @@ hmd_interface_t hmd_rift = {
 
 void VR_OVR_GetFOV(float *fovx, float *fovy)
 {
-	if (vr_ovr_settings.initialized)
-	{
-		float scale = VR_OVR_GetDistortionScale();
-		float yfov = 2 * atan2(vr_ovr_settings.v_screen_size * scale, 2 * vr_ovr_settings.eye_to_screen_distance) * 180/ M_PI;
-		*fovy = yfov;
-		*fovx = yfov * ovrConfig.aspect;
-	}
+	// retrieve the FOV
 }
 
 
 void VR_OVR_GetHMDPos(int32_t *xpos, int32_t *ypos)
 {
-	if (vr_ovr_settings.initialized)
+	if (hmd)
 	{
-		*xpos = vr_ovr_settings.xPos;
-		*ypos = vr_ovr_settings.yPos;
+		*xpos = hmdDesc.WindowsPos.x;
+		*ypos = hmdDesc.WindowsPos.y;
 	}
 }
 
 
 int32_t VR_OVR_getOrientation(float euler[3])
 {
+	/*
 	if (vr_ovr_latencytest->value)
 		LibOVR_ProcessLatencyInputs();
     return LibOVR_GetOrientation(euler);
+	*/
+	return 0;
 }
 
 void VR_OVR_ResetHMDOrientation()
 {
-	LibOVR_ResetHMDOrientation();
+	if (hmd)
+		ovrHmd_ResetSensor(hmd);
 }
 
 int32_t VR_OVR_RenderLatencyTest(vec4_t color) 
 {
-	return (vr_ovr_latencytest->value && LibOVR_GetLatencyTestColor(color));
+//	return (vr_ovr_latencytest->value && LibOVR_GetLatencyTestColor(color));
+	return 0;
 }
 
 void VR_OVR_CalcRenderParam()
 {
+/*
 	if (vr_ovr_settings.initialized)
 	{	
 		float *dk = vr_ovr_settings.distortion_k;
@@ -131,11 +133,13 @@ void VR_OVR_CalcRenderParam()
 		strncpy(ovrConfig.deviceString,vr_ovr_settings.deviceString,sizeof(ovrConfig.deviceString));
 		ovrConfig.projOffset = h;
 	}
+	*/
 }
 
 
 float VR_OVR_GetDistortionScale()
 {
+	/*
 	if (vr_ovr_distortion->value < 0)
 		return 1.0f;
 
@@ -147,39 +151,23 @@ float VR_OVR_GetDistortionScale()
 		return ovrConfig.maxScale;
 	default:
 		return vr_ovr_scale->value;
-	}
+	}*/
+
+	return 0;
 }
 
 int32_t VR_OVR_SetPredictionTime(float time)
 {
-	int32_t result = !!LibOVR_SetPredictionTime(time);
-	if (result)
-		Com_Printf("VR_OVR: Set HMD Prediction time to %.1fms\n", time);
-	return result;
+	prediction_time = time;
+	Com_Printf("VR_OVR: Set HMD Prediction time to %.1fms\n", time);
+	return 1;
 }
 
 void SCR_CenterAlert (char *str);
 void VR_OVR_Frame()
 {
 
-	if (vr_ovr_driftcorrection->modified)
-	{
-		if (vr_ovr_driftcorrection->value)
-		{
-			Cvar_SetInteger("vr_ovr_driftcorrection",1);
-			if (!LibOVR_EnableDriftCorrection())
-			{
-				Com_Printf("VR_OVR: Cannot enable magnetic drift correction - device is not calibrated\n");
-			}
-		}
-		else
-		{
-			Cvar_SetInteger("vr_ovr_driftcorrection",0);
-			LibOVR_DisableDriftCorrection();
-		}
-		vr_ovr_driftcorrection->modified = false;
-	}
-
+/*
 	if (vr_ovr_latencytest->value && LibOVR_IsLatencyTesterAvailable())
 	{
 			const char *results = LibOVR_ProcessLatencyResults();
@@ -196,70 +184,10 @@ void VR_OVR_Frame()
 				}
 			}
 	} 
+*/
+
 }
 
-int32_t VR_OVR_GetSettings(ovr_settings_t *settings)
-{
-	int32_t result = LibOVR_GetSettings(settings);
-	Com_Printf("VR_OVR: Getting HMD settings:");
-	if (result)
-	{
-		Com_Printf(" ok!\n");
-		return 1;
-	}
-
-	Com_Printf(" failed!\n");
-	if (vr_ovr_debug->value)
-	{
-		int32_t riftType = (int32_t) vr_ovr_debug->value;
-		Com_Printf("VR_OVR: Falling back to debug parameters...\n");
-
-		switch (riftType)
-		{
-		case RIFT_DK1:
-			{
-				float distk[4] = { 1.0f, 0.22f, 0.24f, 0.0f };
-				float chrm[4] = { 0.996f, -0.004f, 1.014f, 0.0f };
-				settings->initialized = 1;
-				settings->xPos = 0;
-				settings->yPos = 0;
-				settings->h_resolution = 1280;
-				settings->v_resolution = 800;
-				settings->h_screen_size = 0.14976f;
-				settings->v_screen_size = 0.0936f;
-				settings->interpupillary_distance = 0.064f;
-				settings->lens_separation_distance = 0.0635f;
-				settings->eye_to_screen_distance = 0.041f;
-				memcpy(settings->distortion_k, distk, sizeof(float) * 4);
-				memcpy(settings->chrom_abr, chrm, sizeof(float) * 4);
-				strncpy(settings->deviceString, "Oculus Rift DK1 Emulator", 31);
-			}
-			break;
-		default:
-		case RIFT_DKHD:
-			{
-				float distk[4] = { 1.0f, 0.18f, 0.115f, 0.0f };
-				float chrm[4] = { 0.996f, -0.004f, 1.014f, 0.0f };
-				settings->initialized = 1;
-				settings->xPos = 0;
-				settings->yPos = 0;
-				settings->h_resolution = 1920;
-				settings->v_resolution = 1080;
-				settings->h_screen_size = 0.12096f;
-				settings->v_screen_size = 0.06804f;
-				settings->interpupillary_distance = 0.064f;
-				settings->lens_separation_distance = 0.0635f;
-				settings->eye_to_screen_distance = 0.040f;
-				memcpy(settings->distortion_k, distk, sizeof(float) * 4);
-				memcpy(settings->chrom_abr, chrm, sizeof(float) * 4);
-				strncpy(settings->deviceString, "Oculus Rift DK HD Emulator", 31);
-			}
-			break;
-		}
-		return 1;
-	}
-	return 0;
-}
 
 int32_t VR_OVR_Enable()
 {
@@ -267,7 +195,9 @@ int32_t VR_OVR_Enable()
 	int32_t failure = 0;
 	if (!vr_ovr_enable->value)
 		return 0;
-	if (!LibOVR_DeviceInit())
+	hmd = ovrHmd_Create(0);
+
+	if (!hmd)
 	{
 		Com_Printf("VR_OVR: Error, no HMD detected!\n");
 		failure = 1;
@@ -277,26 +207,41 @@ int32_t VR_OVR_Enable()
 		Com_Printf(" ok!\n");
 	}
 
-	if (failure && !vr_ovr_debug->value)
+	if (failure && vr_ovr_debug->value)
+	{
+		ovrHmdType temp = ovrHmd_None;
+		switch((int) vr_ovr_debug->value)
+		{
+			default:
+				temp = ovrHmd_DK2;
+				break;
+			case 1:
+				temp = ovrHmd_DK1;
+				break;
+			case 2:
+				temp = ovrHmd_DKHD;
+				break;
+		}
+		hmd = ovrHmd_CreateDebug(temp);
+	}
+
+	if (!hmd)
 		return 0;
 
-	// get info from LibOVR
-	if (!VR_OVR_GetSettings(&vr_ovr_settings))
-		return 0;
-
-	Com_Printf("...detected HMD %s with %ux%u resolution\n", vr_ovr_settings.deviceString, vr_ovr_settings.h_resolution, vr_ovr_settings.v_resolution);
-	Com_Printf("...detected IPD %.1fmm\n", vr_ovr_settings.interpupillary_distance * 1000);
-	strncpy(string, va("%.2f", vr_ovr_settings.lens_separation_distance * 1000), sizeof(string));
-	vr_ovr_lensdistance = Cvar_Get("vr_ovr_lensdistance", string, CVAR_ARCHIVE);
+	ovrHmd_GetDesc(hmd, &hmdDesc);
 	
+
+	Com_Printf("...detected HMD %s with %ux%u resolution\n", hmdDesc.ProductName,hmdDesc.Resolution.w, hmdDesc.Resolution.h);
+	
+	if (ovrHmd_StartSensor(hmd,(ovrSensorCap_Orientation | ovrSensorCap_YawCorrection), ovrSensorCap_Orientation))
+	{
+		Com_Printf("...successfully started sensor\n");
+	}
+
 	if (vr_ovr_scale->value < 1)
 		Cvar_SetValue("vr_ovr_scale", 1.0);
 
 	VR_OVR_CalcRenderParam();
-
-	if (vr_ovr_driftcorrection->value > 0.0)
-		if (!LibOVR_EnableDriftCorrection())
-				Com_Printf("VR_OVR: Cannot enable magnetic drift correction - device is not calibrated\n");
 
 	/* 8/30/2013 - never auto enable this.
 	// TODO: use pixel density instead
@@ -306,24 +251,30 @@ int32_t VR_OVR_Enable()
 	Com_Printf("...calculated %.2f minimum distortion scale\n", ovrConfig.minScale);
 	Com_Printf("...calculated %.2f maximum distortion scale\n", ovrConfig.maxScale);
 
-	if (LibOVR_IsLatencyTesterAvailable())
+	if (hmdDesc.SensorCaps & ovrHmdCap_LatencyTest)
 	{
 		Com_Printf("...found latency tester\n");
 		Cvar_SetInteger("vr_ovr_latencytest",1);
 	}
 	
-	return 1;
+	VR_OVR_Disable();
+	return 0;
 }
 
 void VR_OVR_Disable()
 {
-	LibOVR_DeviceRelease();
+	if (hmd)
+	{
+		ovrHmd_StopSensor(hmd);
+		ovrHmd_Destroy(hmd);
+	}
+	hmd = NULL;
 }
 
 
 int32_t VR_OVR_Init()
 {
-	int32_t init = LibOVR_Init();
+	ovrBool init = ovr_Initialize();
 	vr_ovr_supersample = Cvar_Get("vr_ovr_supersample","1.0",CVAR_ARCHIVE);
 	vr_ovr_scale = Cvar_Get("vr_ovr_scale","1.0",CVAR_ARCHIVE);
 	vr_ovr_lensdistance = Cvar_Get("vr_ovr_lensdistance","-1",CVAR_ARCHIVE);
@@ -344,12 +295,12 @@ int32_t VR_OVR_Init()
 
 	Com_Printf("VR_OVR: Oculus Rift support initialized...\n");
 
-	return 1;
+	return 0;
 }
 
 void VR_OVR_Shutdown()
 {
-	LibOVR_Shutdown();
+	ovr_Shutdown();
 }
 
 
