676e4f0cd6087c8d1641fc6310f3c845988073c3
Jared Stafford <jspenguin@jspenguin.org>
2014-10-26 13:29:42 -0500

    Add _GNU_SOURCE so mremap() prototype gets loaded - otherwise, it defaults to a return value of int, too small to store a pointer

--- backends/linux/q_shlinux.c
+++ backends/linux/q_shlinux.c
@@ -17,6 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
+#define _GNU_SOURCE         /* for mremap */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
