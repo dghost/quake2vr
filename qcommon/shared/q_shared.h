091d48a0f8264403ddca064c480b340f38c7f971
Luke Groeninger <luke@dghost.net>
2015-01-22 20:05:04 -0700

    Fix screwed up define

--- qcommon/shared/q_shared.h
+++ qcommon/shared/q_shared.h
@@ -63,7 +63,7 @@ typedef unsigned char uint8_t;
 typedef signed short int16_t;
 typedef unsigned short uint16_t;
 typedef signed int int32_t;
-typedef unsigned int uint32_t_t;
+typedef unsigned int uint32_t;
 typedef signed long long int64_t;
 typedef unsigned long long uint64_t;
 #ifndef _SIZE_T_DEFINED_
