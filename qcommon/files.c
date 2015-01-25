ba9dc640611d1bb3cea8499eca84afddf5374a37
Luke Groeninger <luke@dghost.net>
2015-01-25 00:39:45 -0700

    Upgrade Minizip version + zlib headers
    -Minizip is no longer 10 years old
    -Zlib headers are no longer 10 years old
    -Also moved files out of unix backend and into qcommon/zip
    -Windows needs it too

--- qcommon/files.c
+++ qcommon/files.c
@@ -19,7 +19,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "qcommon.h"
-#include "include/unzip.h"
+#include "zip/unzip.h"
 
 // enables faster binary pak searck, still experimental
 #define BINARY_PACK_SEARCH
