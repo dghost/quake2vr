b6047d5ffc4727aa4c1d6007ff818932e9c2da28
Luke Groeninger <luke@dghost.net>
2015-03-02 03:40:21 -0700

    Update FindOVR.cmake
    -No longer includes hardcoded reference to external/OculusSDK/
    -Now attempts to figure out current build architecture under Linux
    -OVR_INCLUDE_DIR is now exported based on OVH.h
    -OVR_CAPI_INCLUDE_DIR is now exported based on where OVR_CAPI.h is
    
    Primary CMakeLists.txt has been updated to reflect this

--- external/FindOVR.cmake
+++ external/FindOVR.cmake
@@ -1,15 +1,17 @@
 # Locate OVR library
 # This module defines
 # OVR_LIBRARY, the name of the library to link against
-# OVR_INCLUDE_DIR, the directory containing OVR_CAPI.h
+# OVR_INCLUDE_DIR, the directory containing OVR.h
+# OVR_CAPI_INCLUDE_DIR, the directory containing OVR_CAPI.h
 # OVR_FOUND, if false, do not try to link to OVR
 #
-# By default, this looks to see if OculusSDK is in the source directory under external/
+# If a path is set in the OVRDIR environment variable it will check there
+#
+# This has been tested on OS X
+# This may or may not with Linux - it should, but i think it could be broken easily
 #
 
 SET(OVR_SEARCH_PATHS
-  ${CMAKE_CURRENT_SOURCE_DIR}/external/OculusSDK/LibOVR/
-  ${CMAKE_CURRENT_SOURCE_DIR}/external/oculussdk/LibOVR/
   ~/Library/Frameworks
   /Library/Frameworks
   /usr/local
@@ -26,17 +28,26 @@ SET (OVR_SEARCH_PREFIXES
 )
 
 IF (APPLE)
-SET (OVR_SEARCH_PREFIXES ${OVR_SEARCH_PREFIXES}
-	Lib/Mac/Release/
-) 
+	SET (OVR_SEARCH_PREFIXES ${OVR_SEARCH_PREFIXES}
+		Lib/Mac/Release/
+	) 
 ELSEIF (UNIX)
-SET (OVR_SEARCH_PREFIXES ${OVR_SEARCH_PREFIXES}
-	Lib/Linux/Release/i386/
-	Lib/Linux/Release/x86_64/
-) 
-ENDIF (APPLE)
+  IF( CMAKE_SYSTEM_PROCESSOR MATCHES "i.86" )
+    SET( BUILD_ARCH "i386" )
+  ELSEIF( CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+    SET( BUILD_ARCH "x86_64" )
+  ENDIF()
+  SET (OVR_SEARCH_PREFIXES ${OVR_SEARCH_PREFIXES} Lib/Linux/Release/${BUILD_ARCH}/ )
+ENDIF()
 
-FIND_PATH(OVR_INCLUDE_DIR OVR_CAPI.h
+FIND_PATH(OVR_INCLUDE_DIR OVR.h
+  HINTS
+  $ENV{OVRDIR}
+  PATH_SUFFIXES include/ Include/
+  PATHS ${OVR_SEARCH_PATHS}
+)
+
+FIND_PATH(OVR_CAPI_INCLUDE_DIR OVR_CAPI.h
   HINTS
   $ENV{OVRDIR}
   PATH_SUFFIXES include/ Include/ src/ Src/
@@ -50,7 +61,6 @@ FIND_LIBRARY(OVR_LIBRARY_TEMP
   PATH_SUFFIXES ${OVR_SEARCH_PREFIXES}
   PATHS ${OVR_SEARCH_PATHS}
 )
-
 
 IF(OVR_LIBRARY_TEMP)
   # For OS X, OVR uses Cocoa as a backend so it must link to Cocoa.
@@ -71,4 +81,5 @@ ENDIF(OVR_LIBRARY_TEMP)
 
 INCLUDE(FindPackageHandleStandardArgs)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(OVR REQUIRED_VARS OVR_LIBRARY OVR_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OVR REQUIRED_VARS OVR_LIBRARY OVR_INCLUDE_DIR OVR_CAPI_INCLUDE_DIR)
+
